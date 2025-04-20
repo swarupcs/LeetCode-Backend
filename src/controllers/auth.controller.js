ea911aac7c1c8734b188a34b75df9916aa32fee8
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-20 14:43:16 +0530

    check-controller implement

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -128,4 +128,17 @@ export const logout = async (req, res) => {
   }
 };
 
-export const check = async (req, res) => {};
+export const check = async (req, res) => {
+        try {
+          res.status(200).json({
+            success: true,
+            message: "User authenticated successfully",
+            user: req.user,
+          });
+        } catch (error) {
+          console.error("Error checking user:", error);
+          res.status(500).json({
+            error: "Error checking user",
+          });
+        }
+};
