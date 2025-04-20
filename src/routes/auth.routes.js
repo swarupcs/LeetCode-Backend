a51bda7cb4f78df8e63cda7bbdf5f15cf8345e97
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-20 14:37:01 +0530

    implement auth-middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { check, login, logout, register } from "../controllers/auth.controller.js";
+import { authMiddleware } from './../middleware/auth.middleware.js';
 
 const authRoutes = express.Router();
 
@@ -7,8 +8,8 @@ authRoutes.post("/register", register);
 
 authRoutes.post("/login", login);
 
-authRoutes.post("/logout", logout);
+authRoutes.post("/logout", authMiddleware, logout);
 
-authRoutes.get("/check", check);
+authRoutes.get("/check", authMiddleware, check);
 
 export default authRoutes;
