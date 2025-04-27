b63e3a4908e1544ee062a9dde913f262f6148b46
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-27 22:29:22 +0530

    implement problem routes and integrate with middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import problemRoutes from './routes/problem.route.js';
 
 
 dotenv.config();
@@ -17,6 +18,8 @@ app.get('/', (req, res) => {
 });
 
 app.use("/api/v1/auth", authRoutes);
+
+app.use("/api/v1/problems", problemRoutes);
 
 const PORT = process.env.PORT || 8080;
 
