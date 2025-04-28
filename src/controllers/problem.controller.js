3418d3742e878cb238fb81f45933cd69c1031f61
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-04-28 22:45:32 +0530

    complete create problem

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,5 +1,9 @@
 import { db } from "../libs/db.js";
-import { pollBatchResults } from "../libs/judge0.lib.js";
+import {
+  getJudge0LanguageId,
+  pollBatchResults,
+  submitBatch,
+} from "../libs/judge0.lib.js";
 
 export const createProblem = async (req, res) => {
   const {
@@ -15,11 +19,6 @@ export const createProblem = async (req, res) => {
   } = req.body;
 
   // going to check the user role once again
-  if (req.user.role !== "ADMIN") {
-    return res
-      .status(403)
-      .json({ error: "You are not allowed to create a problem" });
-  }
 
   try {
     for (const [language, solutionCode] of Object.entries(referenceSolutions)) {
@@ -47,34 +46,44 @@ export const createProblem = async (req, res) => {
 
       for (let i = 0; i < results.length; i++) {
         const result = results[i];
-
+        console.log("Result-----", result);
+        // console.log(
+        //   `Testcase ${i + 1} and Language ${language} ----- result ${JSON.stringify(result.status.description)}`
+        // );
         if (result.status.id !== 3) {
           return res.status(400).json({
             error: `Testcase ${i + 1} failed for language ${language}`,
           });
         }
       }
-
-      // save the problem to the database;
-
-      const newProblem = await db.problem.create({
-        data: {
-          title,
-          description,
-          difficulty,
-          tags,
-          examples,
-          constraints,
-          testcases,
-          codeSnippets,
-          referenceSolutions,
-          userId: req.user.id,
-        },
-      });
-
-      return res.status(201).json(newProblem);
     }
-  } catch (error) {}
+
+    const newProblem = await db.problem.create({
+      data: {
+        title,
+        description,
+        difficulty,
+        tags,
+        examples,
+        constraints,
+        testcases,
+        codeSnippets,
+        referenceSolutions,
+        userId: req.user.id,
+      },
+    });
+
+    return res.status(201).json({
+      sucess: true,
+      message: "Message Created Successfully",
+      problem: newProblem,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      error: "Error While Creating Problem",
+    });
+  }
 };
 
 export const getAllProblems = async (req, res) => {};
