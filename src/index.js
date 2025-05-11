5e6737ba50668bb1976e62fec81aa3fa05720e6e
Swarup Das <78835981+swarupcs@users.noreply.github.com>
2025-05-11 21:23:36 +0530

    add cors middleware

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
 import problemRoutes from './routes/problem.route.js';
-import executionRoute from "./routes/executeCode.routes.js";
+import executionRoute from './routes/executeCode.routes.js';
 import submissionRoutes from './routes/submission.routes.js';
 import playlistRoutes from './routes/playlist.routes.js';
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,11 +27,11 @@ app.get('/', (req, res) => {
   res.send('Welcome to the leetlab-API!');
 });
 
-app.use("/api/v1/auth", authRoutes);
+app.use('/api/v1/auth', authRoutes);
 
-app.use("/api/v1/problems", problemRoutes);
+app.use('/api/v1/problems', problemRoutes);
 
-app.use("/api/v1/execute-code", executionRoute);
+app.use('/api/v1/execute-code', executionRoute);
 app.use('/api/v1/submission', submissionRoutes);
 app.use('/api/v1/playlist', playlistRoutes);
 
